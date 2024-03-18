7bb7c0642b3bdfdda91aae590ed337eaac2c56e2
Parsa Mostafaie <155385612+parsa-mostafaie@users.noreply.github.com>
2024-03-18 13:01:06 +0330

    Rewrite
    + Icons
    + Future Weather
    + Today Weather
    + Temp
    + Lang: En, Fa, Ar

--- src/js/app.js
+++ src/js/app.js
@@ -1,14 +1,23 @@
 let $ = document;
-let container = $.querySelector("#container > .grid");
+let today_container = $.querySelector(".swiper-wrapper.today");
+let future_container = today_container;
+let swip = $.querySelector(".hide");
+let name = $.querySelector(".name");
+let time_inp = $.querySelector("#time");
 let lang_inp = $.getElementById("lang");
 let city_inp = $.getElementById("city");
-let wday_inp = $.getElementById("wday");
+let swiper;
 
 let city = "";
 let lang = "en";
+let time = 0;
 
 lang_inp.addEventListener("change", () => {
   lang = lang_inp.value;
+});
+
+time_inp.addEventListener("change", () => {
+  time = +time_inp.value;
 });
 
 city_inp.addEventListener("change", () => {
@@ -30,29 +39,96 @@ function fetchData() {
     .then((response) => response.json())
     .then((json) => {
       console.log(json);
+      today_container.innerHTML = "";
+      !time ? setToday(json.days[0]) : setFuture(json.days.slice(1));
+      swip.style.display = "";
+      init_swiper();
+      swiper.slideTo(1);
+      swiper.slidePrev();
     })
     .catch((err) => {
       console.error(err);
     });
 }
 
+function setToday(json) {
+  let frag = $.createDocumentFragment();
+
+  let hours = json.hours;
+
+  for (hour of hours) {
+    let witem = document.createElement("div");
+
+    witem.className = item_classlist;
+    let time = +hour.datetime.slice(0, 2);
+    if (time > 12) {
+      time = time - 12 + " PM";
+    } else {
+      time += " AM";
+    }
+    witem.innerHTML = `
+      <p class="spec-text">${time.padStart(5, "0")}</p>
+      <p>${hour.temp}&deg;C</p>
+      <div style='background: url(images/${
+        hour.icon
+      }.png);background-size: cover;' class='mx-auto w-[32px] h-[32px]' title='${
+      hour.conditions
+    }'></div>
+      `.replace("\n", "");
+
+    frag.appendChild(witem);
+  }
+
+  today_container.append(frag);
+  name.innerHTML = "Today";
+}
+
+function getAday(day, day_id) {
+  let $el = $.createElement("div");
+
+  $el.className = item_classlist;
+  $el.innerHTML = `
+      <i class="spec-text">${
+        day_id == 1 ? "Tomorrow" : day_id + " days after"
+      }</i>
+      <p>${day.temp}&deg;C</p>
+      <div style='background: url(images/${
+        day.icon
+      }.png);background-size: cover;' class='mx-auto w-[32px] h-[32px]' title='${
+    day.conditions
+  }'></div>
+      `.replace("\n", "");
+  return $el;
+}
+
+function setFuture(json) {
+  let frag = $.createDocumentFragment();
+  for (day in json) {
+    frag.append(getAday(json[day], +day + 1));
+  }
+  future_container.append(frag);
+  name.innerHTML = "Future";
+}
+
 // swiper
-const swiper = new Swiper(".swiper", {
-  navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
-  },
-  centeredSlides: true,
-  slidesPerView: 5,
-  spaceBetween: 15,
-  breakpoints: {
-    // when window width is >= 992px
-    992: {
-      slidesPerView: 7,
-      centeredSlides: true,
-      initialSlide: 1,
-      spaceBetween: 25,
+function init_swiper() {
+  const config = {
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
     },
-  },
-  loop: true,
-});
+    slidesPerView: 5,
+    spaceBetween: 15,
+    breakpoints: {
+      // when window width is >= 992px
+      992: {
+        slidesPerView: 7,
+        spaceBetween: 25,
+      },
+    },
+    loop: false,
+  };
+
+  swiper = new Swiper(".swiper", config);
+  return { swiper };
+}
