465f5343bb4835f36244bea58d0932999a3f4f97
Parsa Mostafaie <155385612+parsa-mostafaie@users.noreply.github.com>
2024-03-18 10:42:43 +0330

    Clear To rewrite
    rewrite for better appearance

--- src/js/app.js
+++ src/js/app.js
@@ -30,38 +30,8 @@ function fetchData() {
     .then((response) => response.json())
     .then((json) => {
       console.log(json);
-      renderAday(
-        json.days[wday_inp.value],
-        json.currentConditions,
-        +wday_inp.value == 0
-      );
-      $.querySelector("#city_name").textContent = json.resolvedAddress;
     })
     .catch((err) => {
       console.error(err);
     });
-}
-
-function renderAhour(prev, hour, isnow = false) {
-  return (
-    prev +
-    `<div class="${item_classlist}">
-    <span class="border-b-[#333] border-b-2">${
-      isnow === true ? `<i class='text-[#a0f]'>NOW</i>` : hour.datetime
-    }</span>
-    <span class='border-b-[#333] border-b-2'>${hour.temp}&deg;C</span>
-    <span class='select-all'>${hour.conditions}</span>
-  </div>`
-  );
-}
-
-function renderAday(day, currentConditions, CurrentTime) {
-  container.innerHTML =
-    `<b class='${item_classlist} spec-text'>Sunrise: ${day.sunrise}</b>` +
-    `<b class='${item_classlist} spec-text'>Sunset: ${day.sunset}</b>` +
-    (CurrentTime
-      ? `<b class='${item_classlist} spec-text'>Current Time Zone: ${currentConditions.datetime}</b>` +
-        renderAhour("", currentConditions, true)
-      : "") +
-    day.hours.reduce(renderAhour, "");
 }
